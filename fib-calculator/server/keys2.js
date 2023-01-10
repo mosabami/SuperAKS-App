71f684ee8022c3f27c7c74b72a8e32a82b0ca4b6
Ayobami Ayodeji <ayobaami@hotmail.com>
2023-01-10 03:02:46 -0800

    restore loadtesting

--- fib-calculator/server/keys2.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  redisHost: 'redis-cluster-ip-service',
-  redisPort: '6379',
-  pgUser: 'pguser',
-  pgHost: 'postgres-cluster-ip-service',
-  pgDatabase: 'postgres',
-  pgPassword: 'PgPassword',
-  pgPort: '5432',
-};
--- fib-calculator/worker/keys2.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  redisHost: 'redis-cluster-ip-service',
-  redisPort: '6379',
-};
